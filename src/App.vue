495437d48ce1107c1f4dce5bfd4ba7dbb61200e4
renga-urushihara <urushihara@ren-ga.jp>
2020-05-15 10:30:06 +0900

    test input eve

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
   <div id="app">
     <div>
-      <h3>"keyDown"</h3>
-      <input @keydown="onKeyDown" />
-      <p>{{ this.keyDown }}</p>
-    </div>
-    <div>
-      <h3>"keyPress"</h3>
-      <input @keypress="onKeyPress" />
-      <p>{{ this.keyPress }}</p>
-    </div>
-    <div>
-      <h3>"keyUp"</h3>
-      <input @keyup="onKeyUp" />
-      <p>{{ this.keyUp }}</p>
-    </div>
-    <div>
       <h3>"input"</h3>
       <input @input="onInput" />
-      <p>{{ this.input }}</p>
+      <p>{{ this.inputType }}</p>
+      <p>{{ this.isComposing }}</p>
+      <p>{{ this.data }}</p>
     </div>
   </div>
 </template>
@@ -28,44 +15,16 @@ export default {
   name: "App",
   data: function() {
     return {
-      keyDown: [],
-      keyPress: [],
-      keyUp: [],
-      input: []
+      inputType:"",
+      isComposing: "",
+      data: "",
     };
   },
   methods: {
-    onKeyDown: function(e) {
-      console.log(e);
-      if (e.key === "Backspace") {
-        this.keyDown.pop();
-        return;
-      }
-      this.keyDown.push(e.keyCode);
-    },
-    onKeyPress: function(e) {
-      console.log(e);
-      if (e.key === "Backspace") {
-        this.keyPress.pop();
-        return;
-      }
-      this.keyPress.push(e.keyCode);
-    },
-    onKeyUp: function(e) {
-      console.log(e);
-      if (e.key === "Backspace") {
-        this.keyUp.pop();
-        return;
-      }
-      this.keyUp.push(e.keyCode);
-    },
     onInput: function(e) {
-      console.log(e);
-      if (e.key === "Backspace") {
-        this.input.pop();
-        return;
-      }
-      this.input.push(e.data);
+      this.inputType = e.inputType;
+      this.isComposing = e.isComposing;
+      this.data = e.data;
     }
   }
 };
