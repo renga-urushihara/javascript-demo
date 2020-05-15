4928de213aa366d441105fe483e5fbb5176db692
renga-urushihara <urushihara@ren-ga.jp>
2020-05-15 10:54:08 +0900

    a

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,48 @@
     <div>
       <h3>"input"</h3>
       <input @input="onInput" />
-      <p>inputType: {{ this.inputType }}</p>
-      <p>isComposing: {{ this.isComposing }}</p>
-      <p>data: {{ this.data }}</p>
+      <p>text: {{ this.text }}</p>
+      <p>trackedText: {{ this.trackedText }}</p>
     </div>
   </div>
 </template>
 
 <script>
+const inputTypes = {
+  UNCONVERTED_STATE: "insertCompositionText",
+  CONVERETED_STATE: "insertFromComposition",
+  DELETE_UNCONVERTED: "deleteCompositionText",
+  DELETE_CONVERTED: "deleteContentBackward"
+}
+
 export default {
   name: "App",
   data: function() {
     return {
-      inputType:"",
-      isComposing: "",
-      data: "",
+      text: [],
+      trackedText: []
     };
   },
   methods: {
     onInput: function(e) {
-      this.inputType = e.inputType ? e.inputType : "notype";
-      this.isComposing = e.isComposing ? e.isComposing : "notype";
-      this.data = e.data;
+      switch(e.inputType) {
+        case inputTypes.UNCONVERTED_STATE: {
+          this.text.push(e.data);
+          break;
+        }
+        case inputTypes.CONVERETED_STATE: {
+          this.trackedText = this.text.join("");
+          break;
+        }
+        case inputTypes.DELETE_UNCONVERTED: {
+          this.text.pop();
+          break;
+        }
+        case inputTypes.DELETE_CONVERTED: {
+          this.text.pop();
+          break
+        }
+      }
     }
   }
 };
